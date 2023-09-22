d83d012e2f055308ba35720795759008c743a28d
Akash7126 <akash.ghosh7126@gmail.com>
2023-09-22 12:35:02 +0600

    tttttttttttt

--- Online_Exam_System/Online_Exam_System/Controllers/QuestionController.cs
+++ Online_Exam_System/Online_Exam_System/Controllers/QuestionController.cs
@@ -1,25 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Online_Exam_System.Controllers
 {
     public class QuestionController : Controller
     {
+        private readonly OnlineExamSystemContext _context;
+        public QuestionController(OnlineExamSystemContext context)
+        {
+            _context = context;
+        }
+        //public IActionResult GetCourseCourseAssign()
+        //{
+        //    var courseAssigns = (from courseAssign in _context.CourseAssigns
+        //                         join course in _context.Courses
+        //                         on courseAssign.CourseId equals course.CourseId
+        //                         select new { course.CourseId, course.CourseCode }).Distinct().ToList();
+        //    ViewBag.CourseAssigns = courseAssigns;
+        //    return View();  
+        //}
         public IActionResult QuestionBank()
         {
             return View();
         }
         public IActionResult AddQestion()
         {
+            var courseAssigns = (from courseAssign in _context.CourseAssigns
+                                 join course in _context.Courses
+                                 on courseAssign.CourseId equals course.CourseId
+                                 select new { course.CourseId, course.CourseCode }).Distinct().ToList();
+            ViewBag.CourseAssigns = courseAssigns;
             return View();
         }
 
         public IActionResult AddMultipleSelectQuestion()
         {
+            var courseAssigns1 = (from courseAssign in _context.CourseAssigns
+                                 join course in _context.Courses
+                                 on courseAssign.CourseId equals course.CourseId
+                                 select new { course.CourseId, course.CourseCode }).Distinct().ToList();
+            ViewBag.CourseAssigns1 = courseAssigns1;
             return View();
+            
         }
 
         public IActionResult AddTrueFalseQuestion()
         {
+            var courseAssigns2 = (from courseAssign in _context.CourseAssigns
+                                 join course in _context.Courses
+                                 on courseAssign.CourseId equals course.CourseId
+                                 select new { course.CourseId, course.CourseCode }).Distinct().ToList();
+            ViewBag.CourseAssigns2 = courseAssigns2;
             return View();
         }
     }
--- Online_Exam_System/Online_Exam_System/ViewModel/CourseAssignViewModel.cs
+++ Online_Exam_System/Online_Exam_System/ViewModel/CourseAssignViewModel.cs
@@ -11,7 +11,6 @@ namespace Online_Exam_System.ViewModel
         public int TeacherId { get; set; }
         public int BatchId { get; set; }
         public List<int> StudentList { get; set; }
-        public List<int> CourseList { get; set; }
-        public List<int> BatchList { get; set; }
+       
     }
 }
